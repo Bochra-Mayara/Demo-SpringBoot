9a1602ce305894642cf97c59b67bb43d9eb11cbe
bochra-mayara <bochra-mayara>
2023-08-13 15:35:05 +0200

    App-SpringBoot-Final

--- src/main/java/com/bochra/demo/Student/Student.java
+++ src/main/java/com/bochra/demo/Student/Student.java
@@ -1,71 +1,95 @@
 package com.bochra.demo.Student;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import java.time.LocalDate;
+import java.time.Period;
 
+@Entity
+@Table(name = "student")
 public class Student {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
     private String firstname;
+
     private String lastname;
+
     private LocalDate dateOfBirth;
+
+    @Column(unique = true)
     private String email;
+
+    @Transient
     private int age;
 
-
-
-
     public Student(
-      String firstname,
-      String lastname,
-      LocalDate dateOfBirth,
-      String email,
-      int age
-  ) {
-    this.firstname = firstname;
-    this.lastname = lastname;
-    this.dateOfBirth = dateOfBirth;
-    this.email = email;
-    this.age = age;
-  }
+            String firstname,
+            String lastname,
+            LocalDate dateOfBirth,
+            String email,
+            int age
+    ) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+        this.age = age;
+    }
 
- public Student() {}
+    public Student() {}
 
-  public String getFirstname() {
-    return firstname;
-  }
+    public Integer getId() {
+        return id;
+    }
 
-  public void setFirstname(String firstname) {
-    this.firstname = firstname;
-  }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-  public String getLastname() {
-    return lastname;
-  }
+    public String getFirstname() {
+        return firstname;
+    }
 
-  public void setLastname(String lastname) {
-    this.lastname = lastname;
-  }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
 
-  public LocalDate getDateOfBirth() {
-    return dateOfBirth;
-  }
+    public String getLastname() {
+        return lastname;
+    }
 
-  public void setDateOfBirth(LocalDate dateOfBirth) {
-    this.dateOfBirth = dateOfBirth;
-  }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 
-  public int getAge() {
-    return age;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public void setAge(int age) {
-    this.age = age;
-  }
-    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return Period.between(dateOfBirth, LocalDate.now()).getYears();
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
--- src/main/java/com/bochra/demo/Student/StudentController.java
+++ src/main/java/com/bochra/demo/Student/StudentController.java
@@ -4,6 +4,7 @@ package com.bochra.demo.Student;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,9 @@ public class StudentController {
     
     private StudentService service;
     @Autowired
-    public StudentController(StudentService service){
+    public StudentController(
+            @Qualifier("DBStudentService") StudentService service)
+    {
         this.service=service;
     }
     @PostMapping
